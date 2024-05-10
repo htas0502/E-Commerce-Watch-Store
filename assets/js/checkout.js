273e21dfdbf23dbb55629ce3bdacfbc38b170f71
Nguyen Tuan Khanh <Hoangtuanhsang.5555@gmail.com>
2024-05-11 01:04:28 +0700

    Update Checkout Logic #1

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -135,62 +135,176 @@ renderHeader();
 
 // *****====== CHECKOUT SECTION ======*****
 
-function checkout(event) {
-    // // Ngăn chặn hành động mặc định của form
-    event.preventDefault();
-
-    const totalPriceRender = document.querySelector("#total");
-    const cartBox = document.querySelector(".cartBox");
 
 
-    // user info declared:
-    var name = document.getElementById('name').value;
-    var phone = document.getElementById('phone').value;
-    var address = document.getElementById('address').value;
-    var city = document.getElementById('city').value;
-
-    // Tạo một object mới chứa thông tin khách hàng:
-    var customerObj = {
-        name: name,
-        phone: phone,
-        address: address,
-        city: city
-    }; 
-
-    // // Thêm object customerObj vào mảng customerList (nếu mảng chưa được khởi tạo, cần khởi tạo mảng trước đó)
-    // customerList.push(customerObj);
-
-    // // log test:
-    // console.log("All Users: ", customerList);
-
-
-    const cart = JSON.parse(localStorage.getItem('cart')) || {};
-    localStorage.removeItem('cart'); // Xóa giỏ hàng sau khi thanh toán
+// Render Cart Items:
+document.addEventListener('DOMContentLoaded', function() {
+    // Khai báo biến...
+    const innerCartBox = document.querySelector('.innerCartBox')
+    const cartItem = document.querySelectorAll('.cartItem')
+    const imgCartContainer = document.querySelector('.imgCartContainer')
+    const infoCartContainer = document.querySelector('.infoCartContainer')
+    const quantityContainer = document.querySelector('.quantityContainer')
+    const quantity = document.querySelector('.quantity')
+    const itemQuantity = document.querySelector('.item-quantity')
+    const minusCartBtn = document.querySelector('.minusCartBtn')
+    const addCartBtn = document.querySelector('.addCartBtn')
+    const itemPrice = document.querySelector('.item-price')
+    const cartItemPrice = document.querySelector('.cartItemPrice')
+    const deleteCartItem = document.querySelector('.deleteCartItem')
+    const innerCartTotalPrice = document.querySelector('.innerCartTotalPrice')
+    const coupon = document.querySelector('.coupon')
+    // const preTotalPrice = document.querySelector('.preTotalPrice')
+    const prePrice = document.querySelector('.pre-price')
+    // const totalPrice = document.querySelector('.totalPrice')
+    const price = document.querySelector('.price')
 
 
-    // check if cart is empty:
-    if (cart.length === 0) {
-        alert('Your cart is empty!');
-    } else {
-        // log test:
-        console.log("Cart before checkout: ", cart);
+    let cart = JSON.parse(localStorage.getItem('cart')) || {};
+    // log test
+    // console.log("Product id: ", typeof(Object.keys(cart)));
+    // console.log("Product quantity: ", typeof(Object.values(cart)));
 
-        // Thêm object customerObj vào mảng customerList (nếu mảng chưa được khởi tạo, cần khởi tạo mảng trước đó)
-        customerList.push(customerObj);
-        // log test:
-        console.log("All Users: ", customerList);
+    // ...
+    const cartItemIds = Object.keys(cart);
+    const cartItemQuantities = Object.values(cart);
 
-        // reset cart & total price:
-        alert(`Total amount to pay: ${totalPrice} $ \nThank you for your purchase!`);
-        cart = [];
-        totalPrice = 0;
-    }
+    // log test
+    // console.log(cartItemIds);
+    // console.log(cartItemQuantities);
 
-    // remove cart element after checkout:
-    while (cartBox.hasChildNodes()) {
-        cartBox.removeChild(cartBox.firstChild);
-    }
 
-    // reload total price after checkout:
-    totalPriceRender.innerHTML = `${totalPrice}`;
-}
+    // ...
+    let totalPrice = 0;
+    let totalQuantity = 0
+
+
+    const productsApi = "http://localhost:3000/products";
+        fetch(productsApi)
+            .then((response) => {
+                // JSON.parse:  JSON --> JS type
+                return response.json();
+            })
+            .then((productList) => {
+                productList.forEach((product, product_Key) => {
+                    cartItemIds.forEach((itemId, itemId_Key) => {
+                        cartItemQuantities.forEach((itemQuantity, itemQuantity_Key) => {
+                            if(product.id == itemId) {
+                                if(itemId_Key == itemQuantity_Key) {
+                                    // log test
+                                    // console.log("Item id: ", itemId);
+                                    // console.log("Item quantity: ", itemQuantity);
+
+                                    let productPrice = product.price * itemQuantity;
+                                    totalPrice += productPrice;
+                                    
+                                    console.log("totalPrice: ", totalPrice);
+
+                                    // ...
+                                    const newDiv = document.createElement('div');
+                                    newDiv.classList.add('cartItem');
+                                    newDiv.innerHTML = `
+                                    <!-- img -->
+                                    <div class="imgCartContainer">
+                                        <img src=${product.img} alt="cart_img">
+                                    </div>
+                
+                                    <!-- cart item info -->
+                                    <div class="infoCartContainer">
+                                        <h3>${product.name}</h3>
+                                        <!-- quantity container -->
+                                        <div class="quantityContainer">
+                                            <!-- change quantity -->
+                                            <div class="quantity">
+                                                <span class="item-quantity">
+                                                    <button class="minusCartBtn" onclick="">-</button>
+                                                    <p>${itemQuantity}</p>
+                                                    <button class="addCartBtn" onclick="">+</button>
+                                                </span>
+                                                <span class="item-price">
+                                                    <p>${(productPrice).toLocaleString('vi-VN')} vnd</p>
+                                                </span>
+                                            </div>
+                                        </div>
+                
+                                        <!-- delete btn -->
+                                        <button class="deleteCartItem"><i></i> Xoá</button>
+                                    </div>
+                                    `
+
+                                    // render HTML
+                                    innerCartBox.appendChild(newDiv);
+
+                                    prePrice.textContent = `${totalPrice.toLocaleString('vi-VN')} vnd`;
+                                    price.textContent = `${totalPrice.toLocaleString('vi-VN')} vnd`;
+                                }
+                            }
+
+                        })
+                    })
+                })
+            });
+});
+
+// Pay Checkout:
+// function checkout(event) {
+//     // // Ngăn chặn hành động mặc định của form
+//     event.preventDefault();
+
+//     const totalPriceRender = document.querySelector("#total");
+//     const cartBox = document.querySelector(".cartBox");
+
+
+//     // user info declared:
+//     var name = document.getElementById('name').value;
+//     var phone = document.getElementById('phone').value;
+//     var email = document.getElementById('email').value;
+//     var address = document.getElementById('address').value;
+//     var city = document.getElementById('city').value;
+
+//     // Tạo một object mới chứa thông tin khách hàng:
+//     var customerObj = {
+//         name: name,
+//         phone: phone,
+//         email: phone,
+//         address: address,
+//         city: city
+//     }; 
+
+//     // // Thêm object customerObj vào mảng customerList (nếu mảng chưa được khởi tạo, cần khởi tạo mảng trước đó)
+//     // customerList.push(customerObj);
+
+//     // // log test:
+//     // console.log("All Users: ", customerList);
+
+
+//     const cart = JSON.parse(localStorage.getItem('cart')) || {};
+//     localStorage.removeItem('cart'); // Xóa giỏ hàng sau khi thanh toán
+
+
+//     // check if cart is empty:
+//     if (cart.length === 0) {
+//         alert('Your cart is empty!');
+//     } else {
+//         // log test:
+//         console.log("Cart before checkout: ", cart);
+
+//         // Thêm object customerObj vào mảng customerList (nếu mảng chưa được khởi tạo, cần khởi tạo mảng trước đó)
+//         customerList.push(customerObj);
+//         // log test:
+//         console.log("All Users: ", customerList);
+
+//         // reset cart & total price:
+//         alert(`Total amount to pay: ${totalPrice} $ \nThank you for your purchase!`);
+//         cart = [];
+//         totalPrice = 0;
+//     }
+
+//     // remove cart element after checkout:
+//     while (cartBox.hasChildNodes()) {
+//         cartBox.removeChild(cartBox.firstChild);
+//     }
+
+//     // reload total price after checkout:
+//     totalPriceRender.innerHTML = `${totalPrice}`;
+// }
