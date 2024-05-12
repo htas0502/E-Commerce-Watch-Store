24eee6c5ad1644f4951ef128159e36daf5083a67
Nguyen Tuan Khanh <Hoangtuanhsang.5555@gmail.com>
2024-05-12 14:01:00 +0700

    Update Checkout Logic #4

--- assets/js/checkout.js
+++ assets/js/checkout.js
@@ -10,7 +10,13 @@ const totalPriceRender = document.querySelector("#total");
 const cartBox = document.querySelector(".cartBox");
 const productAmount = document.querySelectorAll("#productAmount");
 const productLinks = document.querySelectorAll('.product_link');
-const alertBox = document.querySelector('.alertBox');
+const brandLinks = document.querySelectorAll('.brand-link');
+const innerCartBox = document.querySelector('.innerCartBox')
+
+const itemCartQuantity = document.querySelectorAll('.itemCartQUantity');
+
+
+let emptyCart = false;
 
 
 // ____________________________________________________________________________________
@@ -24,55 +30,190 @@ function renderHeader() {
     const navTitle = [ 
         {
             id: 1,
-            title: "Thương Hiệu",
+            title: "THƯƠNG HIỆU",
             link: "/pages/brand/brand.html",
         }, 
         {
             id: 2,
-            title: "Nam",
+            title: "NAM",
             link: "/pages/male/male.html",
         }, 
         {
             id: 3,
-            title: "Nữ",
+            title: "NỮ",
             link: "/pages/female/female.html",
         }, 
         {
             id: 4,
-            title: "Phụ Kiện",
+            title: "PHỤ KIỆN",
             link: "/pages/accessory/accessory.html",
         }, 
         {
             id: 5,
-            title: "Liên Hệ",
+            title: "LIÊN HỆ",
             link: "/pages/info/info.html",
         },
     ];
     const dropDownInfo = [
         {
             id: 1,
-            title: "Thương Hiệu",
-            info: `Tùm lum các thương hiệu...`
+            title: "THƯƠNG HIỆU",
+            info: `
+                <div class="brandLayout">
+                    <div class="brand-box-img">
+                        <a class="brand-link" href="/pages/brand/brand.html?brand=Casio">
+                            <img src="https://image.donghohaitrieu.com/wp-content/uploads/2023/10/Casio-1.jpg" alt="casio"/>
+                        </a>
+                    </div>
+                    <div class="brand-box-img">
+                        <a class="brand-link" href="/pages/brand/brand.html?brand=Citizen">
+                            <img src="https://image.donghohaitrieu.com/wp-content/uploads/2023/10/Citizen-1.jpg" alt="citizen"/>
+                        </a>
+                    </div>
+                    <div class="brand-box-img">
+                        <a class="brand-link" href="/pages/brand/brand.html?brand=Doxa">
+                            <img src="https://image.donghohaitrieu.com/wp-content/uploads/2023/10/Doxa.jpg" alt="doxa"/>
+                        </a>
+                    </div>
+                    <div class="brand-box-img">
+                        <a class="brand-link" href="/pages/brand/brand.html?brand=DW">
+                            <img src="https://image.donghohaitrieu.com/wp-content/uploads/2023/10/Daniel-Wellington.jpg" alt="dw"/>
+                        </a>
+                    </div>
+                    <div class="brand-box-img">
+                        <a class="brand-link" href="/pages/brand/brand.html?brand=Fossil">
+                            <img src="https://image.donghohaitrieu.com/wp-content/uploads/2023/10/Fossil.jpg" alt="fossil"/>
+                        </a>
+                    </div>
+                </div>
+                <div class="brandLayout">
+                    <div class="brand-box-img">
+                        <a class="brand-link" href="/pages/brand/brand.html?brand=G-Shock">
+                            <img src="https://image.donghohaitrieu.com/wp-content/uploads/2023/10/G-Shock.jpg" alt="g-shock"/>
+                        </a>
+                    </div>
+                    <div class="brand-box-img">
+                        <a class="brand-link" href="/pages/brand/brand.html?brand=Longines">
+                            <img src="https://image.donghohaitrieu.com/wp-content/uploads/2023/10/Longines.jpg" alt="longines"/>
+                        </a>
+                    </div>
+                    <div class="brand-box-img">
+                        <a class="brand-link" href="/pages/brand/brand.html?brand=Orient">
+                            <img src="https://image.donghohaitrieu.com/wp-content/uploads/2023/10/Orient-1.jpg" alt="orient"/>
+                        </a>
+                    </div>
+                    <div class="brand-box-img">
+                        <a class="brand-link" href="/pages/brand/brand.html?brand=Seiko">
+                            <img src="https://image.donghohaitrieu.com/wp-content/uploads/2023/10/Seiko.jpg" alt="seiko"/>
+                        </a>
+                    </div>
+                    <div class="brand-box-img">
+                        <a class="brand-link" href="/pages/brand/brand.html?brand=Tissot">
+                            <img src="https://image.donghohaitrieu.com/wp-content/uploads/2023/10/Tissot.jpg" alt="tissot"/>
+                        </a>
+                    </div>
+                </div>
+            `
         },
         {
             id: 2,
-            title: "Nam",
-            info: `Các sản phẩm dành cho Nam...`
+            title: "NAM",
+            info: `
+                <div class="content-container">
+                    <a href='/pages/male/male.html'>CÁC SẢN PHẨM DÀNH CHO NAM</a>
+                </div>
+            `
         },
         {
             id: 3,
-            title: "Nữ",
-            info: `Thương hiệu nổi tiếng cho Nữ...`
+            title: "NỮ",
+            info: `
+            <div class="content-container">
+                <a href='/pages/female/female.html'>CÁC SẢN PHẨM DÀNH CHO NỮ</a>
+            </div>
+        `
         },
         {
             id: 4,
-            title: "Phụ Kiện",
-            info: `Mua phụ kiện đeee!`
+            title: "PHỤ KIỆN",
+            info: `
+            <div class="content-container">
+                <a href='/pages/accessory/accessory.html'>PHỤ KIỆN CHO NAM & NỮ</a>
+            </div>
+        `
         },
         {
             id: 5,
-            title: "Liên Hệ",
-            info: `Hãy gọi cho chúng tôi.`
+            title: "LIÊN HỆ",
+            info: `
+            <div class="content-container">
+                <a href='/pages/info/info.html'>THÔNG TIN LIÊN HỆ</a>
+            </div>
+        `
+        },
+    ];
+
+    // Brand Container:
+    const brandList = [
+        {
+            id: 1,
+            title: "Casio",
+            img: "",
+            link: "",
+        },
+        {
+            id: 1,
+            title: "Citizen",
+            img: "",
+            link: "",
+        },
+        {
+            id: 1,
+            title: "Doxa",
+            img: "",
+            link: "",
+        },
+        {
+            id: 1,
+            title: "DW",
+            img: "",
+            link: "",
+        },
+        {
+            id: 1,
+            title: "Fossil",
+            img: "",
+            link: "",
+        },
+        {
+            id: 1,
+            title: "G-Shock",
+            img: "",
+            link: "",
+        },
+        {
+            id: 1,
+            title: "Longines",
+            img: "",
+            link: "",
+        },
+        {
+            id: 1,
+            title: "Orient",
+            img: "",
+            link: "",
+        },
+        {
+            id: 1,
+            title: "Seiko",
+            img: "",
+            link: "",
+        },
+        {
+            id: 1,
+            title: "Tissot",
+            img: "",
+            link: "",
         },
     ];
     
@@ -93,10 +234,7 @@ function renderHeader() {
                 dropDown.classList.add("hide");
                 dropDown.classList.remove("show");
             }
-
-            // dropDown.classList.add("nav_color-change");
         })
-
         navItem.addEventListener("mouseover", () => {
             dropDownInfo.forEach(info => {
                 if(info.id == nav.id) {
@@ -131,6 +269,188 @@ function renderHeader() {
 renderHeader();
 
 
+//  ***** CART CONTAINER *****
+
+// Toggle Cart Btn show/hide event handler:
+cartBtn.addEventListener("click", (e) => {
+    // log test:
+    // console.log('cart');
+
+    if(sidebar.classList.contains("hide")) {
+        sidebar.classList.remove("hide");
+        sidebar.classList.add("show");
+    } else {
+        sidebar.classList.add("hide");
+        sidebar.classList.remove("show");
+    }
+});
+
+// render cart:
+function renderCart() {
+    const cartContainer = document.querySelector(".cartContainer");
+    const cartBox = document.createElement('div');
+    cartBox.innerHTML = `
+        <h2 class="cartTitle">Giỏ Hàng: 0</h2>
+        <ul id="cart"></ul>
+        <div class="cartBox">
+            <h3>Các sản phẩm: </h3>
+        </div>
+        <div class="priceBox">
+            <p></p>
+            <p id="total"></p>
+        </div>
+        <button class="checkoutBtn"><a href="/pages/checkout/checkout.html">Thanh Toán</a></button>
+    `;
+    cartContainer.appendChild(cartBox);
+}
+renderCart();
+
+
+// ____________________________________________________________________________________
+
+
+// *****====== ADD TO CART SECTION ======*****
+
+// Add To Cart Handler:
+function addToCart(productId, productName, productPrice) {
+    // log test
+    console.log(productId, productName, productPrice);
+
+    let cart = JSON.parse(localStorage.getItem('cart')) || {}; // Lấy giỏ hàng từ localStorage
+    cart[productId] = cart[productId] ? cart[productId] + 1 : 1; // Tăng số lượng sản phẩm trong giỏ hàng
+    localStorage.setItem('cart', JSON.stringify(cart)); // Lưu giỏ hàng mới vào localStorage
+
+    // cập nhật lại giỏ hàng
+    updateCartUI(cart, productId, productName, productPrice);
+}
+
+function minusToCart(productId) {
+    let cart = JSON.parse(localStorage.getItem('cart')) || {};
+    const cartTitle = document.querySelector('.cartTitle');
+    const totalPriceElement = document.getElementById('total');
+
+    if (cart[productId] && cart[productId] > 1) {
+        cart[productId]--;
+        localStorage.setItem('cart', JSON.stringify(cart));
+        updateCartUI(cart, productId);
+    } else {
+        delete cart[productId];
+        localStorage.setItem('cart', JSON.stringify(cart));
+        updateCartUI(cart, productId);
+
+        // ...
+        cartTitle.textContent = '';
+        cartTitle.textContent = `Giỏ Hàng: 0`;
+        totalPriceElement.textContent = ``;
+    }
+}
+
+function deleteCart(productId) {
+    const cartTitle = document.querySelector('.cartTitle');
+    const totalPriceElement = document.getElementById('total');
+
+    let cart = JSON.parse(localStorage.getItem('cart')) || {};
+    delete cart[productId];
+    localStorage.setItem('cart', JSON.stringify(cart));
+    updateCartUI(cart, productId);
+
+    // ...
+    cartTitle.textContent = '';
+    cartTitle.textContent = `Giỏ Hàng: 0`;
+    totalPriceElement.textContent = ``;
+}
+
+// Hàm cập nhật giao diện giỏ hàng
+function updateCartUI(cart, productId, productName, productPrice) {
+    const cartItems = document.querySelector('.cartBox');
+    const totalPriceElement = document.getElementById('total');
+    const cartTitle = document.querySelector('.cartTitle');
+    let totalPrice = 0;
+    let totalQuantity = 0;
+
+
+    // Xóa các sản phẩm hiện tại trong giỏ hàng
+    cartItems.innerHTML = '';
+
+    // Hiển thị các sản phẩm trong giỏ hàng và tính tổng tiền
+    for (const productId in cart) {
+        const quantity = cart[productId];
+        // const productPrice = getProductPrice(productId);
+        // const productName = getProductDisplayName(productId);
+
+        const productsApi = "http://localhost:3000/products";
+        fetch(productsApi)
+            .then((response) => {
+                // JSON.parse:  JSON --> JS type
+                return response.json();
+            })
+            .then((productList) => {
+                productList.forEach(dbProduct => {
+                    if(dbProduct.id == productId) {
+                        const product = productList[productId - 1];
+                        totalPrice += dbProduct.price * quantity;
+                        totalQuantity += quantity;
+                        // log
+                        // console.log(totalPrice);
+                        // console.log(totalQuantity);
+
+                        // ...
+                        if(totalQuantity == 0) {
+                            emptyCart = true;
+                        }
+                        console.log("Empty Cart: ", emptyCart);
+
+                        const newDiv = document.createElement('div');
+                        newDiv.classList.add('cartItem')
+                        newDiv.innerHTML = `
+                            <div class="imgCartContainer">
+                                <img src="${dbProduct.img}" alt="img">
+                            </div>
+                            <p dataId=${dbProduct.id} class="productCartInfo">${dbProduct.name}</p>
+                            <p class="productCartPrice">${(dbProduct.price * quantity).toLocaleString('vi-VN')} vnd</p>
+                            <div class="quantityBox">
+                                <div class="quantity">
+                                    <button class="minusCartBtn" onclick="minusToCart(${dbProduct.id})">-</button>
+                                    <p>${quantity}</p>
+                                    <button class="addCartBtn" onclick="addToCart(${dbProduct.id}, '${dbProduct.name}', ${dbProduct.price})">+</button>
+                                </div>
+                                <button class="deleteCartBtn" onclick="deleteCart(${dbProduct.id})">x</button>
+                            </div>
+                        `; 
+                        cartItems.appendChild(newDiv);
+
+                        // ...
+                        cartTitle.textContent = '';
+                        cartTitle.textContent = `Giỏ Hàng: ${totalQuantity}`;
+
+                        // render total price to HTML
+                        let vndPrice = totalPrice.toLocaleString('vi-VN');
+                        totalPriceElement.textContent = `Tổng: ${vndPrice} vnd`;
+                    }
+                })
+            })
+    }
+}
+
+// Hàm reload lại trang ( CHỈ DÙNG TRONG TRANG CHECKOUT! )
+function pageReload() {
+    let cart = JSON.parse(localStorage.getItem('cart')) || {}; // Lấy giỏ hàng từ localStorage
+    console.log(cart);
+
+    if(cart == {}) {
+        console.log("empty cart!");
+    }
+
+    location.reload();
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const cart = JSON.parse(localStorage.getItem('cart')) || {};
+    updateCartUI(cart);
+});
+
+
+
 // ____________________________________________________________________________________
 
 
@@ -159,11 +479,27 @@ document.addEventListener('DOMContentLoaded', function() {
     // const totalPrice = document.querySelector('.totalPrice')
     const price = document.querySelector('.price')
 
+    // declare alert:
+    const emptyCartAlertHtml = document.querySelector('.emptyCartAlert');
+    const alertInfo = document.querySelector('.emptyCartAlert p');
+    const alertLink = document.querySelector('.emptyCartAlert a');
 
+
+    // 
     let cart = JSON.parse(localStorage.getItem('cart')) || {};
-    // log test
-    // console.log("Product id: ", typeof(Object.keys(cart)));
-    // console.log("Product quantity: ", typeof(Object.values(cart)));
+    console.log(cart);
+
+    if(Object.keys(cart).length === 0) {
+        console.log("object empty");
+
+        alertInfo.innerHTML = "*KHÔNG CÓ SẢN PHẨM NÀO TRONG GIỎ HÀNG";
+        alertLink.innerHTML = "VỀ TRANG CHỦ";
+    } else {
+        console.log("object NOT empty");
+    }
+
+
+
 
     // ...
     const cartItemIds = Object.keys(cart);
@@ -202,7 +538,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
                                     // ...
                                     const newDiv = document.createElement('div');
-                                    newDiv.classList.add('cartItem');
+                                    newDiv.classList.add('checkoutCartItem');
                                     newDiv.innerHTML = `
                                     <!-- img -->
                                     <div class="imgCartContainer">
@@ -217,9 +553,9 @@ document.addEventListener('DOMContentLoaded', function() {
                                             <!-- change quantity -->
                                             <div class="quantity">
                                                 <span class="item-quantity">
-                                                    <button class="minusCartBtn" onclick="">-</button>
-                                                    <p>${itemQuantity}</p>
-                                                    <button class="addCartBtn" onclick="">+</button>
+                                                    <button class="minusCartBtn" onclick="minusToCart(${product.id}), pageReload()">-</button>
+                                                    <p class="itemCartQuantity" dataId=${product.id}>${itemQuantity}</p>
+                                                    <button class="addCartBtn" onclick="addToCart(${product.id}, '${product.name}', ${product.price}), pageReload()">+</button>
                                                 </span>
                                                 <span class="item-price">
                                                     <p>${(productPrice).toLocaleString('vi-VN')} vnd</p>
@@ -228,7 +564,7 @@ document.addEventListener('DOMContentLoaded', function() {
                                         </div>
                 
                                         <!-- delete btn -->
-                                        <button class="deleteCartItem"><i></i> Xoá</button>
+                                        <button class="deleteCartItem" onclick="deleteCart(${product.id}), pageReload()"><i></i> Xoá</button>
                                     </div>
                                     `
 
